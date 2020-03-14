407189010bba2225d77f591800ff72c5798bfea7
kantegory <davidobryakov@gmail.com>
2020-03-14 15:33:57 +0300

    Update main.py: Add file2bucket(), update speech2text()

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from __future__ import print_function
 import time
 import boto3
 
-transcribe = boto3.client('transcribe')
-job_name = "job name"
-job_uri = "https://S3 endpoint/test-transcribe/answer2.wav"
+s3 = boto3.resource('s3')
 
-transcribe.start_transcription_job(
-    TranscriptionJobName=job_name,
-    Media={'MediaFileUri': job_uri},
-    MediaFormat='wav',
-    LanguageCode='en-US'
-)
 
-while True:
-    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
-    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-        break
-    print("Not ready yet...")
-    time.sleep(5)
+def speech2text():
+    transcribe = boto3.client('transcribe')
+    job_name = "job name"
+    job_uri = "https://S3 endpoint/test-transcribe/answer2.wav"
 
-print(status)
+    transcribe.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': job_uri},
+        MediaFormat='wav',
+        LanguageCode='en-US'
+    )
+
+    while True:
+        status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet...")
+        time.sleep(5)
+
+    print(status)
+
+
+def file2bucket(filename, bucket):
+    # Upload a new file
+    data = open(filename, 'rb')
+    s3.Bucket(bucket).put_object(Key=filename, Body=data)
