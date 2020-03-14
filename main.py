bf228f2fb809b2aeb32b0c9d05272770df82e06e
kantegory <davidobryakov@gmail.com>
2020-03-14 19:35:15 +0300

    Update main.py, add text2speech()

--- main.py
+++ main.py
@@ -9,9 +9,30 @@ import argparse
 from config import *
 
 
-def get_results(filename, bucket):
+def create_bucket(bucket):
+    # all_buckets = s3.buckets.filter().all()
+    # return all_buckets[0]
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket)
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = e.response['Error']['Code']
+        if error_code == '404':
+            exists = False
+
+    return bucket
+
+
+def file2bucket(filename, bucket):
+    # Upload a new file
+    data = open(filename, 'rb')
+    s3.Bucket(bucket).put_object(Key=filename, Body=data)
+
+
+def speech2text(filename, bucket):
     transcribe = session.client('transcribe')
-    job_name = "jobname{}".format(filename)
+    job_name = "jobname12{}".format(filename)
     job_uri = "https://s3.{}.amazonaws.com/{}/{}".format(CONFIG['region_name'], bucket, filename)
 
     transcribe.start_transcription_job(
@@ -34,50 +55,47 @@ def get_results(filename, bucket):
 
     r = requests.get(link)
     result = json.loads(r.text)
-    result = result['results']['items']
+    # result = result['results']['items']
+    result = result['results']['transcripts'][0]['transcript']
     print(result)
 
     return result
 
 
-def file2bucket(filename, bucket):
-    # Upload a new file
-    data = open(filename, 'rb')
-    s3.Bucket(bucket).put_object(Key=filename, Body=data)
+def text2speech(text):
+    polly = session.client('polly')
+
+    response = polly.start_speech_synthesis_task(
+        VoiceId='Joanna',
+        OutputS3BucketName='mlhack',
+        OutputS3KeyPrefix='key',
+        OutputFormat='mp3',
+        Text=text
+    )
+
+    taskId = response['SynthesisTask']['TaskId']
+
+    while True:
+        status = polly.get_speech_synthesis_task(TaskId=taskId)
+        if status in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet text2speech...")
+        time.sleep(5)
+
+    print(status)
+    return status
 
 
-def create_bucket(bucket):
-    # all_buckets = s3.buckets.filter().all()
-    # return all_buckets[0]
-    try:
-        s3.meta.client.head_bucket(Bucket=bucket)
-    except botocore.exceptions.ClientError as e:
-        # If a client error is thrown, then check that it was a 404 error.
-        # If it was a 404 error, then the bucket does not exist.
-        error_code = e.response['Error']['Code']
-        if error_code == '404':
-            exists = False
-
-    return bucket
-
-    # if (all_buckets) == 0:
-    #     s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': CONFIG['region_name']})
-    #     return all_buckets[0]
-    #
-    # else:
-    #     return all_buckets[0]
-
-
-def speech2text(filename):
+def remove_accent(filename):
     bucket = create_bucket('mlhack')
 
     # uploading file to aws bucket
     file2bucket(filename, bucket)
 
     # getting result of speech2text
-    result = get_results(filename, bucket)
-
-    return result
+    speech2text_result = speech2text(filename, bucket)
+    text2speech_result = text2speech(speech2text_result)
+    # return result
 
 
 if __name__ == "__main__":
@@ -92,4 +110,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Write path to file')
     parser.add_argument('filename', type=str)
     filename = vars(parser.parse_args())['filename']
-    speech2text(filename)
+    remove_accent(filename)
